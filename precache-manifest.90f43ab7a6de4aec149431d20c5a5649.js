d9ac4602a063b1183493dac3cd73a89143dea877
RapiDash <srivatsa.m11@gmail.com>
2020-04-06 23:02:04 +0530

    Updates

---
+++ precache-manifest.90f43ab7a6de4aec149431d20c5a5649.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f483c28073fc24e98fae5d781dddc22c",
+    "url": "/Fractals/index.html"
+  },
+  {
+    "revision": "c7904287df7fb3b9b7ff",
+    "url": "/Fractals/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f45846c88e4b0e693359",
+    "url": "/Fractals/static/css/main.c273f5de.chunk.css"
+  },
+  {
+    "revision": "c7904287df7fb3b9b7ff",
+    "url": "/Fractals/static/js/2.f062d16f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Fractals/static/js/2.f062d16f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f45846c88e4b0e693359",
+    "url": "/Fractals/static/js/main.7c116ac3.chunk.js"
+  },
+  {
+    "revision": "177831da80f5f38f6a36",
+    "url": "/Fractals/static/js/runtime-main.07b392f1.js"
+  }
+]);
